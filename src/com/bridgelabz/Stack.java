38b17f1cd64a55969d142a2de022d43367d0fa19
Javul Mulla <javulmulla8@gmail.com>
2022-08-19 19:48:19 +0530

    Added: stack operations

--- src/com/bridgelabz/Stack.java
+++ src/com/bridgelabz/Stack.java
@@ -1,5 +1,13 @@
 package com.bridgelabz;
-
+import com.bridgelabz.LinkedList;
 public class Stack {
-
+public static void main(String[] args) {
+	LinkedList list = new LinkedList();
+	list.push(70);
+	list.push(30);
+	list.push(56);
+	list.show();
 }
+
+	}
+
