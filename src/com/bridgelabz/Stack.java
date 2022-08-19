b199e59fcfbeb52dd1bcc6e1104dbc457d7f00b9
Javul Mulla <javulmulla8@gmail.com>
2022-08-19 19:57:10 +0530

    Added: Added push method to create stack

--- src/com/bridgelabz/Stack.java
+++ src/com/bridgelabz/Stack.java
@@ -1,13 +1,23 @@
 package com.bridgelabz;
+import java.util.Scanner;
 import com.bridgelabz.LinkedList;
+
 public class Stack {
-public static void main(String[] args) {
-	LinkedList list = new LinkedList();
-	list.push(70);
-	list.push(30);
-	list.push(56);
-	list.show();
-}
+	public static void main(String[] args) {
+		LinkedList list = new LinkedList();
+		Scanner sc = new Scanner(System.in);
+		System.out.println(
+				"Choose option to perform task:" + "1. Create a stack");
+		int option = sc.nextInt();
+		switch (option) {
+			case 1 :
+				list.push(70);
+				list.push(30);
+				list.push(56);
+				list.show();
+				break;
+		}
 
 	}
 
+}
